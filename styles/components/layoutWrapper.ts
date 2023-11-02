c27bcd2e4a2fa35a00705fc1e85bdfc6ffef14bf
kjkim <kjkim@xsinc.co.kr>
2023-11-02 18:57:49 +0900

    desigin: 레이아웃 상하단 높이 조절

--- styles/components/layoutWrapper.ts
+++ styles/components/layoutWrapper.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const LayoutWrapper = styled.div`
   width: 100%;
-  padding-top: 60px;
+  padding-top: 64px;
   display: flex;
   flex-direction: column;
 
@@ -20,6 +20,7 @@ export const LayoutWrapper = styled.div`
   }
 
   footer {
+    margin-top: 64px;
     width: 100%;
     height: 200px;
     background-color: #032541;
