66f5321f0e120ca74d11845260f02a2c8d619251
kjkim <kjkim@xsinc.co.kr>
2023-11-06 16:06:07 +0900

    refactor: api 연동 로직 임시 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import { useEffect } from "react";
 import Image from "next/image";
 import SearchBar from "./index/searchBar";
 import { IndexWrapper } from "@/styles/pages/index/indexWrapper";
 import Link from "next/link";
 import HomeFilterBar from "./index/homeFilterBar";
+import axios from "axios";
+import ContentLayout from "@/components/contentLayout";
 
 export default function Home() {
   const contentsArr = [
@@ -34,55 +37,73 @@ export default function Home() {
     },
   ];
 
+  const headers = {
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN}`,
+    accept: "application/json",
+  };
+
+  // useEffect(() => {
+  //   axios
+  //     .get("https://api.themoviedb.org/3/tv/popular?language=ko&page=1", {
+  //       headers,
+  //     })
+  //     .then((res) => {
+  //       // const data = response.data.json();
+  //       console.log(res.data, "TMDB 데이터");
+  //     });
+  // }, []);
+
   return (
-    <IndexWrapper>
-      <SearchBar />
-      {contentsArr.map((val, idx) => {
-        const { category, className, filter } = val;
+    <ContentLayout>
+      <IndexWrapper>
+        <SearchBar />
+        {contentsArr.map((val, idx) => {
+          const { category, className, filter } = val;
 
-        //임시 데이터 복사 로직
-        const dummyData = [];
+          //임시 데이터 복사 로직
+          const dummyData = [];
 
-        for (let i = 0; i < 30; i++) {
-          dummyData.push({
-            title: "베니스 유령 살인 사건",
-            image: "/images/testContentImg.jpg",
-            release: "9월 13, 2023",
-            score: "68%",
-          });
-        }
-        // 임시 데이터 복사 로직
+          for (let i = 0; i < 30; i++) {
+            dummyData.push({
+              title: "베니스 유령 살인 사건",
+              image: "/images/testContentImg.jpg",
+              release: "9월 13, 2023",
+              score: "68%",
+            });
+          }
+          // 임시 데이터 복사 로직
 
-        return (
-          <div className={`content ${className}`} key={`${category}${idx}`}>
-            <div className="titleBar">
-              <h2>{category}</h2>
-              <HomeFilterBar filter={filter} />
+          return (
+            <div className={`content ${className}`} key={`${category}${idx}`}>
+              <div className="titleBar">
+                <h2>{category}</h2>
+                <HomeFilterBar filter={filter} />
+              </div>
+              <ul className="contentList">
+                {dummyData &&
+                  dummyData.map((val, idx) => {
+                    const { title, image, release, score } = val;
+
+                    return (
+                      <li key={`${title}${idx}`}>
+                        <Link href="">
+                          <div className="contentImg">
+                            <Image src={image} fill alt="contentImg" />
+                          </div>
+                          <div className="score">{score}</div>
+                          <div className="titleRelease">
+                            <div className="title">{title}</div>
+                            <div className="release">{release}</div>
+                          </div>
+                        </Link>
+                      </li>
+                    );
+                  })}
+              </ul>
             </div>
-            <ul className="contentList">
-              {dummyData &&
-                dummyData.map((val, idx) => {
-                  const { title, image, release, score } = val;
-
-                  return (
-                    <li key={`${title}${idx}`}>
-                      <Link href="">
-                        <div className="contentImg">
-                          <Image src={image} fill alt="contentImg" />
-                        </div>
-                        <div className="score">{score}</div>
-                        <div className="titleRelease">
-                          <div className="title">{title}</div>
-                          <div className="release">{release}</div>
-                        </div>
-                      </Link>
-                    </li>
-                  );
-                })}
-            </ul>
-          </div>
-        );
-      })}
-    </IndexWrapper>
+          );
+        })}
+      </IndexWrapper>
+    </ContentLayout>
   );
 }
