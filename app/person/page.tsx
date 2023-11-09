c6c6afad4d93fb73cff0c24005840ea08af14164
kjkim <kjkim@xsinc.co.kr>
2023-11-09 19:34:03 +0900

    refactor: personPage api 연동

--- app/person/page.tsx
+++ app/person/page.tsx
@@ -1,42 +1,50 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import ContentLayout from "@/components/contentLayout";
+import Link from "next/link";
 import Image from "next/image";
 
+import { apiClient } from "@/api/httpClient";
+import ContentLayout from "@/components/contentLayout";
+
 export default function PersonPage() {
-  const [dummyData, setDummyData] = useState<any>([]);
+  const [personData, setPersonData] = useState<any>([]);
 
   useEffect(() => {
-    const dummy = {
-      name: "Tom Holland",
-      sub: " 스파이더맨: 홈커밍, 스파이더맨: 노 웨이 홈, and 스파이더맨: 파 프롬 홈",
-      image: "/images/testContentImg3.jpg",
-    };
-
-    const arr = [];
-
-    for (let i = 0; i < 20; i++) {
-      arr.push(dummy);
-    }
-
-    setDummyData(arr);
+    apiClient.get("person/popular?language=ko&page=1").then((res) => {
+      setPersonData(res.data.results);
+    });
   }, []);
 
   return (
     <ContentLayout>
       <div className="categoryTitle">인기 인물</div>
       <div className="personList">
-        {dummyData.map((val: any, idx: number) => {
-          const { name, sub, image } = val;
+        {personData.map((val: any) => {
+          const { id, name, known_for, profile_path } = val;
           return (
-            <div key={`${val}${idx}`} className="personCard">
-              <div className="personImg">
-                <Image src={image} fill alt="personImg" />
-              </div>
+            <div key={id} className="personCard">
+              <Link href={`/personDetail?id=${id}`}>
+                <div className="personImg">
+                  <Image
+                    src={`https://image.tmdb.org/t/p/w235_and_h235_face/${profile_path}`}
+                    fill
+                    sizes="1x"
+                    alt="personImg"
+                  />
+                </div>
+              </Link>
               <div className="personInfo">
-                <div className="name">{name}</div>
-                <div className="sub">{sub}</div>
+                <Link href={`/personDetail?id=${id}`} className="name">
+                  {name}
+                </Link>
+                <div className="sub">
+                  {known_for.map((val: any, idx: number, arr: any) => {
+                    const title = val.title || val.name;
+
+                    return `${title}${idx !== arr.length - 1 ? ", " : ""}`;
+                  })}
+                </div>
               </div>
             </div>
           );
