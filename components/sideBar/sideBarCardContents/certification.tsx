3f9057b198115ffffb1517bc770f5dd6a8e16386
kjkim <kjkim@xsinc.co.kr>
2023-11-15 19:04:26 +0900

    refactor: Certification 컴포넌트 수정

--- components/sideBar/sideBarCardContents/certification.tsx
+++ components/sideBar/sideBarCardContents/certification.tsx
@@ -3,20 +3,22 @@ import { useState } from "react";
 import CategoryButtons from "@/components/categoryButtons";
 import { usePathname } from "next/navigation";
 
-export default function Certification() {
+export default function Certification({ setValue }: any) {
   const pathname = usePathname();
   const [certificationArr, setCertification] = useState<any>([
-    { category: "Exempt", checked: false },
-    { category: "ALL", checked: false },
-    { category: "7", checked: false },
-    { category: "12", checked: false },
-    { category: "15", checked: false },
-    { category: "19", checked: false },
+    { id: "Exempt", name: "Exempt", checked: false },
+    { id: "ALL", name: "ALL", checked: false },
+    { id: "7", name: "7", checked: false },
+    { id: "12", name: "12", checked: false },
+    { id: "15", name: "15", checked: false },
+    { id: "19", name: "19", checked: false },
   ]);
 
   const onClickSelect = (idx: number, checked: boolean) => {
     const copy = [...certificationArr];
     copy[idx].checked = !checked;
+
+    setValue("certification", copy, { shouldDirty: true });
     setCertification(copy);
   };
 
