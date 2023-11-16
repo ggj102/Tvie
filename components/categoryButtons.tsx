20217a210ba305eccc5f6a3d5c2e59490d6e6603
kjkim <kjkim@xsinc.co.kr>
2023-11-16 17:00:34 +0900

    fix: 보류 컴포넌트 주석

--- components/categoryButtons.tsx
+++ components/categoryButtons.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { CategoryButtonsWrapper } from "@/styles/components/sideBar/sideBarCardContents/categoryButtonsWrapper";
 
 export type CategoryDataType = {
--- components/sideBar/sideBarCardContents/certification.tsx
+++ components/sideBar/sideBarCardContents/certification.tsx
@@ -1,36 +1,36 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-import CategoryButtons, {
-  CategoryDataType,
-} from "@/components/categoryButtons";
-import { usePathname } from "next/navigation";
+// import CategoryButtons, {
+//   CategoryDataType,
+// } from "@/components/categoryButtons";
+// import { usePathname } from "next/navigation";
 
-export default function Certification() {
-  const pathname = usePathname();
-  const [certificationArr, setCertification] = useState<CategoryDataType[]>([
-    { id: 1, name: "Exempt", checked: false },
-    { id: 0, name: "ALL", checked: false },
-    { id: 7, name: "7", checked: false },
-    { id: 12, name: "12", checked: false },
-    { id: 15, name: "15", checked: false },
-    { id: 19, name: "19", checked: false },
-  ]);
+// export default function Certification() {
+//   const pathname = usePathname();
+//   const [certificationArr, setCertification] = useState<CategoryDataType[]>([
+//     { id: 1, name: "Exempt", checked: false },
+//     { id: 0, name: "ALL", checked: false },
+//     { id: 7, name: "7", checked: false },
+//     { id: 12, name: "12", checked: false },
+//     { id: 15, name: "15", checked: false },
+//     { id: 19, name: "19", checked: false },
+//   ]);
 
-  const onClickSelect = (idx: number, checked: boolean) => {
-    const copy = [...certificationArr];
-    copy[idx].checked = !checked;
-    setCertification(copy);
-  };
+//   const onClickSelect = (idx: number, checked: boolean) => {
+//     const copy = [...certificationArr];
+//     copy[idx].checked = !checked;
+//     setCertification(copy);
+//   };
 
-  return (
-    <div className="cardContent">
-      <div>Certification</div>
-      {pathname === "/tv" && (
-        <CategoryButtons
-          categoryData={certificationArr}
-          onClickSelect={onClickSelect}
-        />
-      )}
-    </div>
-  );
-}
+//   return (
+//     <div className="cardContent">
+//       <div>Certification</div>
+//       {pathname === "/tv" && (
+//         <CategoryButtons
+//           categoryData={certificationArr}
+//           onClickSelect={onClickSelect}
+//         />
+//       )}
+//     </div>
+//   );
+// }
