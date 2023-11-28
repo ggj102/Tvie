85ff2e1e82f023ee1bfe64091aa13bb02dbade41
kjkim <kjkim@xsinc.co.kr>
2023-11-28 18:06:49 +0900

    fix: [SearchResults] setCurrentTab 예외처리

--- app/searchResults/components/searchResultsTab/useSearchResultsTab.ts
+++ app/searchResults/components/searchResultsTab/useSearchResultsTab.ts
@@ -26,9 +26,11 @@ export default function useSearchResultsTab() {
 
   useEffect(() => {
     const split = pathname.split("/");
-    const replace = split[2].replace("Search", "");
+    if (split.length > 2) {
+      const replace = split[2].replace("Search", "");
 
-    setCurrentTab(replace);
+      setCurrentTab(replace);
+    }
   }, [pathname]);
 
   useEffect(() => {
