38826bafb85d0e058010613aeb6193125570fce2
kjkim <kjkim@xsinc.co.kr>
2023-12-26 17:31:41 +0900

    refactor: rootLayout serverProps 로직 추가 - 세션 관련 데이터 요청 로직

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,51 @@
 import type { Metadata } from "next";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { getManagementUser, initUser } from "../api/authZero";
+
 import "./globals.css";
 
-import Layout from "@/components/rootLayout";
 import Navigation from "@/components/navigation";
+import MainLayout from "@/components/mainLayout";
+
+import StoreProvider from "./StoreProvider";
 
 export const metadata: Metadata = {
   title: "TVie",
   description: "TV, Movie search",
 };
 
-export default function RootLayout({
+async function ServerSideProps() {
+  const userData = await getManagementUser();
+  let initData = userData;
+
+  if (!userData?.user_metadata.favorites && userData) {
+    initData = await initUser(userData);
+  }
+
+  let sessionState = !!userData;
+
+  return { initData, sessionState };
+}
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { initData, sessionState } = await ServerSideProps();
+
   return (
     <html lang="en">
-      <body>
-        <Navigation />
-        <Layout>{children}</Layout>
-      </body>
+      <UserProvider>
+        <body>
+          <StoreProvider>
+            <MainLayout>
+              <Navigation sessionState={sessionState} />
+              {children}
+            </MainLayout>
+          </StoreProvider>
+        </body>
+      </UserProvider>
     </html>
   );
 }
