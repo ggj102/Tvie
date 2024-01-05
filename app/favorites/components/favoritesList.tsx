ac5da41c8ce5e313b0bba80557b55ded5c406483
kjkim <kjkim@xsinc.co.kr>
2024-01-05 11:12:55 +0900

    refactor: 즐겨찾기 데이터가 없을 경우 예외처리

--- app/favorites/components/favoritesList.tsx
+++ app/favorites/components/favoritesList.tsx
@@ -24,67 +24,71 @@ export default function FavoritesList({
     <div className={favoritesStyle.favorites_list}>
       <div>{titleConnectObj[type]}</div>
       <ul>
-        {list.map((val: any) => {
-          const { id, vote_average } = val;
+        {list.length > 0 ? (
+          list.map((val: any) => {
+            const { id, vote_average } = val;
 
-          const title = val.title || val.name;
-          const imagePath = val.profile_path || val.poster_path;
-          const imageSize =
-            type === "person" ? "w235_and_h235" : "w220_and_h330";
-          const date = val.release_date || val.first_air_date;
-          const dateFormat = dateFormatter(date);
-          const link =
-            type === "person"
-              ? `personDetail?id=${id}`
-              : `contentsDetail?type=${type}&id=${id}`;
+            const title = val.title || val.name;
+            const imagePath = val.profile_path || val.poster_path;
+            const imageSize =
+              type === "person" ? "w235_and_h235" : "w220_and_h330";
+            const date = val.release_date || val.first_air_date;
+            const dateFormat = dateFormatter(date);
+            const link =
+              type === "person"
+                ? `personDetail?id=${id}`
+                : `contentsDetail?type=${type}&id=${id}`;
 
-          return (
-            <li key={id}>
-              <Link href={`/${link}`}>
-                <CustomImage
-                  className={favoritesStyle.image}
-                  type={type}
-                  src={`https://image.tmdb.org/t/p/${imageSize}_face/${imagePath}`}
-                />
-              </Link>
-              {type === "person" ? (
-                <div className={favoritesStyle.person_info}>
-                  <Link
-                    href={`/personDetail?id=${id}`}
-                    className={favoritesStyle.person_name}
-                  >
-                    {title}
-                  </Link>
-                </div>
-              ) : (
-                <>
-                  <VoteAverage
-                    size={34}
-                    top={256}
-                    left={8}
-                    vote={vote_average}
+            return (
+              <li key={id}>
+                <Link href={`/${link}`}>
+                  <CustomImage
+                    className={favoritesStyle.image}
+                    type={type}
+                    src={`https://image.tmdb.org/t/p/${imageSize}_face/${imagePath}`}
                   />
-                  <div className={favoritesStyle.title_release}>
-                    <Link href={`/${link}`} className={favoritesStyle.title}>
+                </Link>
+                {type === "person" ? (
+                  <div className={favoritesStyle.person_info}>
+                    <Link
+                      href={`/personDetail?id=${id}`}
+                      className={favoritesStyle.person_name}
+                    >
                       {title}
                     </Link>
-                    <div className={favoritesStyle.release}>{dateFormat}</div>
                   </div>
-                </>
-              )}
-              <div className={favoritesStyle.delete_btn}>
-                <button
-                  title="즐겨찾기 해제"
-                  onClick={() => onClickDelete(id, type, title)}
-                >
-                  <RemoveCircleIcon
-                    style={{ color: "#d72828", fontSize: "30px" }}
-                  />
-                </button>
-              </div>
-            </li>
-          );
-        })}
+                ) : (
+                  <>
+                    <VoteAverage
+                      size={34}
+                      top={256}
+                      left={8}
+                      vote={vote_average}
+                    />
+                    <div className={favoritesStyle.title_release}>
+                      <Link href={`/${link}`} className={favoritesStyle.title}>
+                        {title}
+                      </Link>
+                      <div className={favoritesStyle.release}>{dateFormat}</div>
+                    </div>
+                  </>
+                )}
+                <div className={favoritesStyle.delete_btn}>
+                  <button
+                    title="즐겨찾기 해제"
+                    onClick={() => onClickDelete(id, type, title)}
+                  >
+                    <RemoveCircleIcon
+                      style={{ color: "#d72828", fontSize: "30px" }}
+                    />
+                  </button>
+                </div>
+              </li>
+            );
+          })
+        ) : (
+          <div>즐겨찾는 {titleConnectObj[type]}이(가) 없습니다.</div>
+        )}
       </ul>
     </div>
   );
